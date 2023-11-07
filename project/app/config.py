0c16860ab9f38e252a70bd49b752f316be03f0c6
Sinan Goktas <sinan.goktas@gmail.com>
2023-11-07 19:58:29 +0000

    first commit

--- project/app/config.py
+++ project/app/config.py
@@ -1,15 +1,14 @@
 import logging
 from functools import lru_cache
 
-from pydantic import BaseSettings
-
+from pydantic import BaseSettings, AnyUrl
 
 log = logging.getLogger("uvicorn")
-
 
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = 0
+    database_url: AnyUrl = None
 
 @lru_cache()
 def get_settings() -> BaseSettings:
