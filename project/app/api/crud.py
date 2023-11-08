f5a50c00064eec069127d0fc7de50496743c7308
Sinan Goktas <sinan.goktas@gmail.com>
2023-11-08 23:21:42 +0000

    more routers

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,5 +1,6 @@
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+from typing import Union, List
 
 async def post(payload: SummaryPayloadSchema) -> int:
     summary = TextSummary(
@@ -8,3 +9,13 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
