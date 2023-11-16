4b0b52d7e8ccbd7c0c5efd6d59a4c3ded3f4bbc7
Sinan Goktas <sinan.goktas@gmail.com>
2023-11-16 00:00:50 +0000

    imports fixed_2

--- project/tests/__init__.py
+++ project/tests/__init__.py
@@ -1,4 +1,1 @@
-from starlette.testclient import TestClient
-from app.main import create_application
-from app.config import get_settings, Settings
-from tortoise.contrib.fastapi import register_tortoise
+
