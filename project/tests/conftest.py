5c20964650df9512734c510ec9fc3fcbacdfa6b1
sinangoktas <sinan.goktas@gmail.com>
2025-05-02 17:48:29 +0100

    requirements update

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,8 +3,8 @@ import os
 import pytest
 from starlette.testclient import TestClient
 
-from app.main import create_application
-from app.config import get_settings, Settings
+from project.app.main import create_application
+from project.app.config import get_settings, Settings
 
 from tortoise.contrib.fastapi import register_tortoise
 
